ba6ff2d0a6a5119a2f8be04be12c9d75ae53053b
Lijo Daniel <Lijo.daniel@everbridge.com>
2026-05-10 17:29:10 +0530

    remove skills progress bar section

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,22 +130,7 @@
    */
   new PureCounter();
 
-  /**
-   * Animate the skills items on reveal
-   */
-  let skillsAnimation = document.querySelectorAll('.skills-animation');
-  skillsAnimation.forEach((item) => {
-    new Waypoint({
-      element: item,
-      offset: '80%',
-      handler: function(direction) {
-        let progress = item.querySelectorAll('.progress .progress-bar');
-        progress.forEach(el => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%';
-        });
-      }
-    });
-  });
+  // Skills animation removed (skills section deleted)
 
   /**
    * Initiate glightbox
